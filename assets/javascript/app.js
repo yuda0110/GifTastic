f8e76c497d3e348840a3b6d23347b97ff70c3509
Yuko Uda <yuko.uda0110@gmail.com>
2020-03-15 20:47:16 -0700

    Create gifTastic object and in the obj, create characters array and renderButtons method

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,35 @@
 // http://api.giphy.com/v1/gifs/search?api_key=1sjuds6ZKI5gKnDVhsyFsT55ptYVnuUG&q=mickey%20mouse&limit=5
+
+$(document).ready(function () {
+
+  const gifTastic = {
+    characters: [
+      'Mickey Mouse',
+      'Goofy',
+      'Minnie Mouse',
+      'Donald Duck',
+      'Elsa',
+      'Ariel',
+      'Belle'
+    ],
+    // Ex) Anna, Tiana, Sheriff Woody, Buzz Lightyear, Jasmine, Daisy Duck, Pluto
+
+    renderButtons: function () {
+      $('#buttons-view').empty();
+
+      this.characters.forEach(function (char) {
+        const btnChar = $('<button>');
+        btnChar.addClass('btn-char');
+        btnChar.attr('data-name', char);
+        btnChar.text(char);
+        $('#buttons-view').append(btnChar);
+      });
+    }
+
+  };
+
+  gifTastic.renderButtons();
+
+
+
+});
